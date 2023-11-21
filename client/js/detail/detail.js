59bf7e63938521382c904fe7ea0f17a4656cd7c2
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-11-21 19:45:26 -0300

    Actualizacion proyect

---
+++ client/js/detail/detail.js
@@ -0,0 +1,59 @@
+const queryString = location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+//llamadas de funciones y eventos
+getData();
+function getData() {
+  const url = "https://successful-bear-bandanna.cyclic.app/api/events/" + id;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const dataEvents = data.eventos;
+      const createCard = document.getElementById("detailsContainer");
+      
+      let date = new Date(dataEvents.eventDate);
+      var dataPrint = date.toISOString().split('T')[0].split('-').reverse().join('-');
+      createCard.innerHTML = `
+            <div class="imagen-central-evento">
+        <img src="${dataEvents.eventImg}" 
+        alt="${dataEvents.title}" class="imagen-recortada">
+    </div>
+        <div class="row">
+            <!-- Autores: Foto y nombre -->
+            <div class="col-12 col-md-6 m-2 d-flex align-items-center">
+                <img src="${dataEvents.createdBy.userImg}" alt="Foto de perfil del autor/a" class="mr-3 img-profile">
+                <div class="texto">
+                    <h6>${dataEvents.createdBy.name}</h6>
+                </div>
+            </div>
+            <!-- Titulo del evento -->
+            <div class="col-12 col-md-6 m-2">
+                <div class="subtitle-detail">
+                    <h4>${dataEvents.title}</h4>
+                </div>
+            </div>
+            <!-- Botones de fecha, categoría y ubicación del evento -->
+            <div class="col-12 col-md-6 m-2">
+                <button class="btn btn-success me-2">${dataPrint}</button>
+                <button class="btn btn-success me-2">${dataEvents.category}</button>
+                <button class="btn btn-outline-success">${dataEvents.location}</button>
+            </div>
+            <!-- Contendio principal y explicación del evento -->
+            <div class="parrafo m-2 mt-4">
+                <p>${dataEvents.description}</p>
+            </div>
+            <!-- Botón asistir. redirigir a la pagina de inscripción -->
+            <div class="m-2">
+                <a class="btn btn-outline-success" href="#">ASISTIR</a>
+            </div>
+            <!-- Mapa de Google para ubicación-->
+            <div class=" mt-5" style="text-align: center;">
+                <iframe src="${dataEvents.map}" 
+                width="100%" height="383" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+            `;
+    });
+}
---
+++ client/js/events/events-main.js
@@ -0,0 +1,98 @@
+
+const cardContainer = document.getElementById("cardContainer");
+const input = document.querySelector("input");
+const favoriteIcon = document.querySelector(".favorite-icon");
+let dataEvents;
+// funciones
+
+function getData() {
+  const url = "https://successful-bear-bandanna.cyclic.app/api/events";
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      dataEvents = data.eventos; // Use data.eventos instead of data.events
+      createCards(data.eventos); // Use data.eventos instead of data.events
+      input.addEventListener("input", (e) => {
+        e.preventDefault();
+        let oneFilter = textFilter(data.eventos, input.value.toLowerCase()); // Use data.eventos instead of data.events
+      });
+    });
+}
+function textFilter(array, text) {
+  const dataEvents = array;
+  let textFilter = dataEvents.filter((element) => {
+    return element.title.toLowerCase().includes(text);
+  });
+  createCards(textFilter);
+  return textFilter;
+}
+
+function createCards(data) {
+  let card = ``;
+  const mapdata = data.map(element => {
+    const favoriteLocalStore = localStorage.getItem("favorite");
+    let isFavorite = false;
+
+    if (favoriteLocalStore !== null) {
+      const parsedFavorite = JSON.parse(favoriteLocalStore);
+      isFavorite = parsedFavorite.some((element2) => element2._id === element._id);
+    }
+
+    card += `
+      <div class="col">
+        <div class="card h-100 custom-card">
+          <div class="city-button">
+            <button type="button" class="btn btn-success">${element.location}</button>
+          </div>
+          <img src="${element.eventImg}" class="card-img-top card-image" alt="limpiando el mar">
+          <div class="card-body ">
+            <div class="d-flex justify-content-between align-items-center">
+              <h5 class="card-title">${element.title}</h5>
+              <button onClick="toggleFavorite('${element._id}')">
+                <i class="favorite-icon ${isFavorite?(" bi bi-heart-fill text-danger"):("bi bi-heart")}" id="heartIcon_${element._id}"></i>
+              </button>
+            </div>
+            <p class="text-success">01/12/2023</p>
+            <p class="card-text">${element.description}</p>
+          </div>
+          <div class="d-flex justify-content-center">
+            <button onclick="probando()" class="btn btn-outline-success" href="#">ASISTIR</button>
+            <div style="width: 50px;"></div>
+            <a class="btn btn-success" href="detail.html?id=${element._id}">VER MÁS</a>
+          </div>
+        </div>
+      </div>`;
+  });
+  cardContainer.innerHTML = card;
+}
+getData();
+
+
+const toggleFavorite = (id) => {
+    const heartIcon = document.getElementById(`heartIcon_${id}`);
+
+    let favoriteLocalStore = localStorage.getItem("favorite");
+
+    if (favoriteLocalStore === null) {
+      favoriteLocalStore = "[]"; 
+    }
+    const parsedFavorite = JSON.parse(favoriteLocalStore);
+    const isFavorite = parsedFavorite.some((element) => element._id === id);
+  
+
+    if (isFavorite) {
+      const updatedFavorites = parsedFavorite.filter((element) => element._id !== id);
+      localStorage.setItem("favorite", JSON.stringify(updatedFavorites));
+      heartIcon.classList.remove("bi-heart-fill", "text-danger", "bi");
+        heartIcon.classList.add("bi","bi-heart");
+
+    } else {
+      const dataFilter = dataEvents.find((element) => element._id === id);
+      heartIcon.classList.remove("bi-heart");
+        heartIcon.classList.add("bi","bi-heart-fill", "text-danger");
+      if (dataFilter) {
+        parsedFavorite.push(dataFilter);
+        localStorage.setItem("favorite", JSON.stringify(parsedFavorite));
+      }
+    }
+  };
