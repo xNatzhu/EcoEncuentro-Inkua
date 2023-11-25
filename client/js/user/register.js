bf80cd18af038f43120c7b349000abc9172dd55a
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-11-25 08:13:00 -0300

    Rollback

---
+++ client/js/user/register.js
@@ -0,0 +1,58 @@
+const registerForm = document.getElementById("formRegister");
+const token = sessionStorage.getItem("token");
+
+registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const params = {
+        "name": username,
+        "email": email,
+        "password": password
+    };
+
+    registerUser(params);
+});
+
+
+function registerUser(params) {
+    const URL = "https://successful-bear-bandanna.cyclic.app/api/users/create";
+
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params),
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.log(data);
+            console.log("Error en la solicitud");
+            throw new Error('Error en la solicitud');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        console.log('Respuesta del servidor:', data);
+        Swal.fire({
+            icon: "success",
+            title: "Usuario registrado exitosamente",
+            confirmButtonColor: "#8db596"
+        });  
+        sessionStorage.setItem("token", data.token);
+        // Aquí puedes manejar la respuesta del servidor
+    })
+    .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
+        Swal.fire({
+            icon: "error",
+            title: `${data.message}`,
+            confirmButtonColor: "#f27474"
+        });
+        // Aquí puedes manejar errores en la solicitud
+    });
+}
